efda90351b5e99c5577fb7b222d54dcc01323545
MorpheusByte <tahasaidcakmak@icloud.com>
2025-04-04 20:40:41 +0300

    1

--- selectors.js
+++ selectors.js
@@ -6,6 +6,24 @@ document.title = "FS19"
 //*            GETELEMENTBYID()
 //*===========================================
 
+console.log(document.getElementById("add-new-item"));
+
+// id si add-new-item olan elemanı seçer
+const headerText = document.getElementById("add-new-item")
+
+
+//! seçilen elemanın style ını manipüle ettik
+// herşey key-value şeklinde yazılır
+headerText.style.color = "blue"
+headerText.style.backgroundColor = "orange"
+headerText.style.border = "2px solid black"
+
+//! Note property'lerde kebap-case yerine camel-case notasyonu ile yazılır
+
+
+const addBtn = document.getElementById("btn")
+console.log(addBtn);
+
 
 //*===========================================
 //*          GETELEMENTSBYTAGNAME()
